c2fb0d3e7b63cce58382b0914e059a4474810e95
nyawox <nyawox.git@gmail.com>
2024-12-06 02:38:55 +0900

    improve the readability and clean the session file properly

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
 use anyhow::{Context, Result};
-use niri_ipc::{Action, Reply, Request, Response, Window, socket::Socket};
+use niri_ipc::{Action, Reply, Request, Response, Window, WorkspaceReferenceArg, socket::Socket};
 use std::{fs, path::PathBuf, sync::Arc};
-use tokio::select;
-use tokio::signal::unix::{SignalKind, signal};
-use tokio::spawn;
-use tokio::sync::Notify;
+use tokio::{
+    select,
+    signal::unix::{SignalKind, signal},
+    spawn,
+    sync::Notify,
+    time::Duration,
+    time::sleep,
+};
 
-/// Fetch the list of windows.
+/// Fetch the windows list
 async fn get_niri_windows() -> Result<Vec<Window>> {
-    let socket = Socket::connect().context("failed to connect to niri ipc socket")?;
-    let (reply, _event) = socket
+    let socket = Socket::connect().context("Failed to connect to Niri IPC socket")?;
+    let (reply, _) = socket
         .send(Request::Windows)
-        .context("failed to retrieve windows from niri ipc")?;
+        .context("Failed to retrieve windows from Niri IPC")?;
 
     match reply {
         Reply::Ok(Response::Windows(windows)) => Ok(windows),
-        Reply::Err(error_msg) => anyhow::bail!("niri ipc returned an error: {}", error_msg),
-        _ => anyhow::bail!("Unexpected reply type from niri"),
+        Reply::Err(error_msg) => anyhow::bail!("Niri IPC returned an error: {}", error_msg),
+        _ => anyhow::bail!("Unexpected reply type from Niri"),
     }
 }
 
+/// fetch the session file path
 fn get_session_file_path() -> Result<PathBuf> {
     let mut session_dir =
         dirs::data_dir().context("Failed to locate data directory (XDG_DATA_HOME)")?;
@@ -28,6 +33,7 @@ fn get_session_file_path() -> Result<PathBuf> {
     Ok(session_dir.join("session.json"))
 }
 
+/// Save the session to a file
 async fn save_session(file_path: &PathBuf) -> Result<()> {
     let windows = get_niri_windows().await?;
     let json_data =
@@ -37,7 +43,7 @@ async fn save_session(file_path: &PathBuf) -> Result<()> {
     Ok(())
 }
 
-/// Restore a session state from a file.
+/// Restore saved session and clean it up
 async fn restore_session(file_path: &PathBuf) -> Result<()> {
     if !file_path.exists() {
         println!("No previous session found at {}", file_path.display());
@@ -48,13 +54,12 @@ async fn restore_session(file_path: &PathBuf) -> Result<()> {
     let windows: Vec<Window> =
         serde_json::from_str(&session_data).context("Failed to parse session JSON")?;
 
-    let mut handles = vec![];
+    let current_windows = get_niri_windows().await?;
+    let mut handles = Vec::new();
 
-    for window in &windows {
-        // probably only useful when restarting systemd service
+    for window in windows {
         if let Some(app_id) = &window.app_id {
-            if get_niri_windows()
-                .await?
+            if current_windows
                 .iter()
                 .any(|w| w.app_id == Some(app_id.clone()))
             {
@@ -62,53 +67,57 @@ async fn restore_session(file_path: &PathBuf) -> Result<()> {
             }
         }
 
-        let app_id = window.app_id.clone().unwrap_or_else(|| "".to_string());
+        let app_id = window.app_id.clone().unwrap_or_default();
         let workspace_id = window.workspace_id;
+
         let handle = spawn(async move {
-            let spawn_socket = Socket::connect().context("failed to connect to niri ipc socket")?;
-            let (spawn_reply, _event) = spawn_socket
+            let spawn_socket = Socket::connect().context("Failed to connect to Niri IPC socket")?;
+            let (reply, _) = spawn_socket
                 .send(Request::Action(Action::Spawn {
                     command: vec![app_id.clone()],
                 }))
                 .context("Failed to send spawn request")?;
 
-            if let Reply::Ok(Response::Handled) = spawn_reply {
-                let move_socket =
-                    Socket::connect().context("failed to connect to niri ipc socket")?;
+            if let Reply::Ok(Response::Handled) = reply {
                 for _ in 0..10 {
-                    tokio::time::sleep(std::time::Duration::from_millis(500)).await;
-                    if let Ok(current_windows) = get_niri_windows().await {
-                        if let Some(new_window) = current_windows
-                            .iter()
-                            .find(|w| w.app_id == Some(app_id.clone()))
-                        {
-                            let move_request = Request::Action(Action::MoveWindowToWorkspace {
+                    sleep(Duration::from_millis(500)).await;
+                    let new_windows = get_niri_windows().await?;
+                    if let Some(new_window) = new_windows
+                        .iter()
+                        .find(|w| w.app_id == Some(app_id.clone()))
+                    {
+                        let move_socket =
+                            Socket::connect().context("Failed to connect to Niri IPC socket")?;
+                        let _ = move_socket
+                            .send(Request::Action(Action::MoveWindowToWorkspace {
                                 window_id: Some(new_window.id),
-                                reference: niri_ipc::WorkspaceReferenceArg::Id(
-                                    workspace_id.expect("no workspace id"),
+                                reference: WorkspaceReferenceArg::Id(
+                                    workspace_id.unwrap_or_default(),
                                 ),
-                            });
-                            let _ = move_socket
-                                .send(move_request)
-                                .context("Failed to move window to the workspace")?;
-                            break;
-                        }
+                            }))
+                            .context("Failed to move window to the workspace")?;
+                        break;
                     }
                 }
             } else {
-                println!("Failed to spawn app: {:?}", app_id);
+                println!("Failed to spawn app: {}", app_id);
             }
+
             Result::<()>::Ok(())
         });
+
         handles.push(handle);
     }
 
-    // Wait for all window spawning and moving tasks to complete
+    // Wait for all tasks to complete.
     for handle in handles {
-        let _ = handle.await.context("Failed to join handle")?;
+        handle.await.context("Task execution failed")??;
     }
 
     println!("Session restored.");
+    // Clean up the session file after restoring.
+    fs::remove_file(file_path).context("Failed to delete session file")?;
+    println!("Session file cleaned up.");
     Ok(())
 }
 
@@ -131,13 +140,11 @@ async fn main() -> Result<()> {
     let shutdown_signal = Arc::new(Notify::new());
 
     restore_session(&session_file_path).await?;
+    let shutdown_signal_clone = Arc::clone(&shutdown_signal);
+    handle_shutdown_signals(shutdown_signal_clone).await;
 
-    handle_shutdown_signals(Arc::clone(&shutdown_signal)).await;
-
-    // Wait for SIGTERM, SIGINT, or SIGQUIT
+    // Wait for shutdown signal
     shutdown_signal.notified().await;
-
     save_session(&session_file_path).await?;
-
     Ok(())
 }
